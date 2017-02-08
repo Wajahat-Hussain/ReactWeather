9279cdb359f5a3075c42682fbd37b1a5e44fba20
Asad Marfani <Asad Marfani>
2017-02-09 00:08:50 +0500

    'AddedCopy'

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
 	return (
+		<div>	
 			<h2>Example Component!</h2>
+			<p>Welcome to Example Component</p>
+		</div>
 		);
 }
 module.exports = Examples;
